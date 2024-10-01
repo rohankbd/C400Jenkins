ed6f65e35fb306a981eef633bfe5afd12fba9f17
Rohan Kabadi <86970070+rohankbd@users.noreply.github.com>
2024-10-01 16:56:11 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static void main() {
+  public static void main(String[] args) {
     System.out.print("You are learning Jenkins today! May the force be with you!");
   }
 }
